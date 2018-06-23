730e5bb6fad8c4b7bab3c5781600f1d6e35fe90e
Tiến Chiêu Đặng <dangtien2409@gmail.com>
2018-06-23 11:08:47 +0700

    dtc-23-06-2019

--- DATH-paingain/src/main/java/DAO/CategoryDAO.java
+++ DATH-paingain/src/main/java/DAO/CategoryDAO.java
@@ -84,15 +84,8 @@ public class CategoryDAO {
 
 	    public PgCategories getCategory(int id) {
 	        //Session session = Hibernate.getSessionFactory().openSession();
-	        Session session ;
-		 	if(Hibernate.getSessionFactory().getCurrentSession() != null)
-		 	{
-		 		session = Hibernate.getSessionFactory().getCurrentSession();
-		 	}
-		 	else
-		 	{
-		 		session = Hibernate.getSessionFactory().openSession();
-		 	}
+	        Session session = Hibernate.getSessionFactory().openSession();
+		 	
 	        session.beginTransaction();
 	        PgCategories category = (PgCategories) session.get(PgCategories.class, id);
 	        session.getTransaction().commit();
